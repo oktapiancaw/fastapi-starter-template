47c60ddc4406eab3d4c3d9ed11481e09de4d6478
Oktapian Candra <oktapian1998@gmail.com>
2024-07-02 15:03:16 +0700

    perf: :zap: Split connection into factory, for flexibility of use

--- src/configs/db.py
+++ src/configs/db.py
@@ -1,26 +1,47 @@
+from contextlib import contextmanager
+
 from loguru import logger
 from pymongo import MongoClient
 
 from src.common import envs
-from src.models.base import ConnectionMeta
+from src.models.base import DatabaseConnectionMeta
 
 
-class MainMongo:
-    def __init__(self, **kwargs) -> None:
-        self.metadata: ConnectionMeta = ConnectionMeta(
-            host=envs.MONGO_HOST,
-            port=envs.MONGO_PORT,
-            database=envs.MONGO_DATABASE,
-        )
+class MongoConnector:
+    def __init__(self, meta: DatabaseConnectionMeta) -> None:
+        self._meta: DatabaseConnectionMeta = meta
+        if not self._meta.uri:
+            self._meta.uri = self._meta.uri_string(base="mongodb", with_db=False)
+        self.client = None
 
+    @contextmanager
+    def __call__(self, **kwargs):
+        self.connect(**kwargs)
+        yield self
+        self.close()
+
+    def connect(self, **kwargs):
         try:
-            self.client: MongoClient = MongoClient(
-                self.metadata.url(base="mongodb", with_db=False), **kwargs
-            )
-            self.conn = self.client[self.metadata.database]
+            self.client: MongoClient = MongoClient(self._meta.uri, **kwargs)
+            self.db = self.client[self._meta.database]
+            logger.success("Database is connected")
         except:
-            logger.warning("Fail connect to mongo")
             raise
 
     def close(self):
-        self.client.close()
+        if self.client:
+            self.client.close()
+        logger.success("Database is closed")
+
+
+class MainMongo(MongoConnector):
+    def __init__(self) -> None:
+        super().__init__(
+            meta=DatabaseConnectionMeta(
+                host=envs.MONGO_HOST,
+                port=envs.MONGO_PORT,
+                username=envs.MONGO_USERNAME,
+                password=envs.MONGO_PASSWORD,
+                database=envs.MONGO_DATABASE,
+            )
+        )
--- src/models/base.py
+++ src/models/base.py
@@ -1,20 +1,48 @@
+import re
+
 from typing import Optional, Union
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, model_validator
 
 
-class ConnectionMeta(BaseModel):
+
+class DatabaseConnectionMeta(BaseModel):
+
     host: Optional[str] = Field("localhost", description="Connection host")
-    port: Optional[int] = Field(8000, description="Connection port")
+    port: Optional[Union[str, int]] = Field(8000, description="Connection port")
     username: Optional[str] = Field(None)
     password: Optional[str] = Field(None)
     database: Optional[Union[str, int]] = Field(None, description="Database name")
 
-    def url(self, base: str = "http", with_db: bool = True) -> str:
-        if self.username:
-            return f"{base}://{self.username}:{self.password}@{self.host}:{self.port}/{self.database if with_db else ''}"
-        return f"{base}://{self.host}:{self.port}/{self.database if with_db else ''}"
+    uri: Optional[str] = Field("", description="")
 
+    def uri_string(self, base: str = "http", with_db: bool = True) -> str:
+        meta = f"{self.host}:{self.port}"
+        if self.username:
+            return f"{base}://{self.username}:{self.password}@{meta}/{self.database if with_db else ''}"
+        return f"{base}://{meta}/{self.database if with_db else ''}"
+
+    @model_validator(mode="after")
+    def extract_uri(self):
+        if self.uri:
+            uri = re.sub(r"\w+:(//|/)", "", self.uri)
+            metadata, others = (
+                re.split(r"\/\?|\/", uri) if re.search(r"\/\?|\/", uri) else [uri, None]
+            )
+            if others and "&" in others:
+                for other in others.split("&"):
+                    if "=" in other and re.search(r"authSource", other):
+                        self.database = other.split("=")[-1]
+                    elif "=" not in other:
+                        self.database = other
+            if "@" in metadata:
+                self.username, self.password, self.host, self.port = re.split(
+                    r"\@|\:", metadata
+                )
+            else:
+                self.host, self.port = re.split(r"\:", metadata)
+            self.port = int(self.port)
+        return self
 
 class SearchSchema(BaseModel):
     field: str = Field(..., description="Field name")
