a09375399e8aaaacb0f9bd8d2906d55c9b51ca46
Oktapian Candra <oktapian1998@gmail.com>
2024-07-02 15:04:24 +0700

    perf: :zap: Standardization response, model, routes

--- src/models/post.py
+++ src/models/post.py
@@ -2,13 +2,25 @@ from uuid import uuid4
 from typing import Optional
 from datetime import datetime
 
-from pydantic import BaseModel, Field
+from fastapi import Response
+from pydantic import BaseModel, RootModel, Field, ConfigDict
 
 
 class PostMeta(BaseModel):
     title: str = Field(..., description="Title post")
     content: str = Field(..., description="Content post")
     published: Optional[bool] = Field(default=True, description="Published post")
+
+    model_config = ConfigDict(
+        extra="forbid",
+        json_schema_extra={
+            "example": {
+                "title": "Title post",
+                "content": "Content post",
+                "published": True,
+            }
+        },
+    )
 
 
 class PostData(PostMeta):
@@ -21,18 +33,18 @@ class PostData(PostMeta):
     deletedAt: Optional[int] = Field(None)
 
     @property
-    def save_mongo(self) -> dict:
+    def save_mongo(self):
         return self.model_dump(by_alias=True, exclude_none=True)
 
     @property
-    def update_mongo(self) -> dict:
+    def update_mongo(self):
         self.createdAt = int(datetime.now().timestamp() * 1000)
         return self.model_dump(
             by_alias=True, exclude_none=True, exclude={"id", "createdAt"}
         )
 
     @property
-    def update_mongo(self) -> dict:
+    def delete_mongo(self):
         self.deletedAt = int(datetime.now().timestamp() * 1000)
         self.status = "archive"
         return self.model_dump(
@@ -40,5 +52,20 @@ class PostData(PostMeta):
         )
 
 
-class PostView(PostData):
-    ...
+class PostView(PostData): ...
+
+
+class PostViewList(RootModel):
+    root: list[PostView]
+
+    def __iter__(self):
+        return iter(self.root)
+
+    def __getitem__(self, item):
+        return self.root[item]
+
+    def __len__(self):
+        return self.root.__len__()
+
+
+# PostViewList = RootModel[list[PostView]]
--- src/routes/api/post.py
+++ src/routes/api/post.py
@@ -2,128 +2,221 @@ from typing import cast
 
 from loguru import logger
 from pydantic import TypeAdapter
-from fastapi import APIRouter, Request, Path, HTTPException
+from fastapi import APIRouter, Request, HTTPException, Path, status
 from fastapi.responses import JSONResponse
 
 from src.configs.db import MainMongo
 from src.models.base import SearchSchema
-from src.models.post import PostMeta, PostData, PostView
+from src.models.post import PostMeta, PostData, PostView, PostViewList
 
 app = APIRouter()
+__main_collection = "post"
 
 
-@app.get("/get-one/{id}")
+@app.get("/{id}")
 def get_post(req: Request, id: str = Path(...)):
     try:
         mongo = cast(MainMongo, req.app.state.mongo)
 
-        raw = mongo.conn["template"].find_one({"_id": id, "status": {"$ne": "archive"}})
+        raw = mongo.db[__main_collection].find_one(
+            {"_id": id, "status": {"$ne": "archive"}}
+        )
         if raw:
             payload = TypeAdapter(PostView).validate_python(raw)
-            return payload
+            return JSONResponse(
+                status_code=status.HTTP_200_OK,
+                content={
+                    "status": status.HTTP_200_OK,
+                    "message": "Success get all post",
+                    "data": payload.model_dump(),
+                },
+            )
 
-        return JSONResponse(status_code=404, content="Data is not found")
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={
+                "status": status.HTTP_404_NOT_FOUND,
+                "message": "Post is not found",
+            },
+        )
+
     except Exception:
         logger.opt(exception=Exception).error("Failed get post")
-        return HTTPException(status_code=500, detail="Failed get post")
+        return HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Failed get post"
+        )
 
 
-@app.get("/get-all")
+@app.get("s")
 def get_all_posts(req: Request):
     try:
+
         # ? Get mongo from lifespan
         mongo = cast(MainMongo, req.app.state.mongo)
 
+        raw = mongo.db[__main_collection].find({"status": {"$ne": "archive"}})
         # * Validation if post is exists
-        raw = mongo.conn["template"].find({"status": {"$ne": "archive"}})
-        posts = list(raw)
-        if posts:
+        if list(raw):
             # * Transform post model
-            payloads = TypeAdapter(list[PostView]).validate_python(posts)
-            return payloads
+            payloads = TypeAdapter(PostViewList).validate_python(raw)
+            return JSONResponse(
+                status_code=status.HTTP_200_OK,
+                content={
+                    "status": status.HTTP_200_OK,
+                    "message": "Success get all post",
+                    "data": payloads.model_dump(),
+                    "metadata": {"size": payloads.__len__()},
+                },
+            )
 
-        return JSONResponse(status_code=404, content="Data is not found")
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={
+                "status": status.HTTP_404_NOT_FOUND,
+                "message": "Post is not found",
+            },
+        )
+
     except Exception:
         logger.opt(exception=Exception).error("Failed get all post")
-        return HTTPException(status_code=500, detail="Failed get all post")
+        return HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Failed get all post",
+        )
 
 
-@app.post("/search")
+@app.post("s/search")
 def search_posts(req: Request, search: SearchSchema):
     try:
         mongo = cast(MainMongo, req.app.state.mongo)
-
-        raw = mongo.conn["template"].find(
-            {
-                "status": {
-                    "$ne": "archive",
-                },
-                search.field: {"$regex": search.value},
-            }
+        query = {"status": {"$ne": "archive"}}
+        count = mongo.db[__main_collection].count_documents(query)
+        raw = mongo.db[__main_collection].find(
+            query.update({search.field: {"$regex": search.value}})
         )
-        posts = list(raw)
-        if posts:
-            payloads = TypeAdapter(list[PostView]).validate_python(posts)
-            return payloads
 
-        return JSONResponse(status_code=404, content="Data is not found")
+        # * Validation if post is exists
+        if list(raw):
+            # * Transform post model
+            payloads = TypeAdapter(PostViewList).validate_python(raw)
+            return JSONResponse(
+                status_code=status.HTTP_200_OK,
+                content={
+                    "status": status.HTTP_200_OK,
+                    "message": "Success get all post",
+                    "data": payloads.model_dump(),
+                    "metadata": {"size": count, "totalItems": payloads.__len__()},
+                },
+            )
+
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={
+                "status": status.HTTP_404_NOT_FOUND,
+                "message": "Post is not found",
+            },
+        )
+
     except Exception:
         logger.opt(exception=Exception).error("Failed get all post")
-        return HTTPException(status_code=500, detail="Failed get all post")
+        return HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Failed get all post",
+        )
 
 
-@app.post("/add")
+@app.post("")
 def add_post(req: Request, data: PostMeta):
     try:
         mongo = cast(MainMongo, req.app.state.mongo)
 
         payload = TypeAdapter(PostData).validate_python(data.model_dump())
 
-        mongo.conn["template"].insert_one(payload.save_mongo)
+        mongo.db[__main_collection].insert_one(payload.save_mongo)
 
-        return payload.id
+        return JSONResponse(
+            status_code=status.HTTP_201_CREATED,
+            content={
+                "status": status.HTTP_201_CREATED,
+                "message": "Success add post",
+                "data": {"id": payload.id},
+            },
+        )
     except Exception:
         logger.opt(exception=Exception).error("Failed add post")
-        return HTTPException(status_code=500, detail="Failed add post")
+        return HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Failed add post"
+        )
 
 
-@app.put("/edit/{id}")
+@app.put("/{id}")
 def edit_post(req: Request, data: PostMeta, id: str = Path(...)):
     try:
         mongo = cast(MainMongo, req.app.state.mongo)
 
-        raw = mongo.conn["template"].find_one({"_id": id})
+        raw = mongo.db[__main_collection].find_one({"_id": id})
         if raw:
             payload = TypeAdapter(PostData).validate_python(data.model_dump())
 
-            mongo.conn["template"].update_one(
+            mongo.db[__main_collection].update_one(
                 {"_id": id}, {"$set": payload.update_mongo}
             )
 
-            return payload.id
+            return JSONResponse(
+                status_code=status.HTTP_202_ACCEPTED,
+                content={
+                    "status": status.HTTP_202_ACCEPTED,
+                    "message": "Success edit post",
+                    "data": {"id": payload.id},
+                },
+            )
 
-        return JSONResponse(status_code=404, content="Data is not found")
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={
+                "status": status.HTTP_404_NOT_FOUND,
+                "message": "Post is not found",
+            },
+        )
     except Exception:
         logger.opt(exception=Exception).error("Failed edit post")
-        return HTTPException(status_code=500, detail="Failed edit post")
+        return HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Failed edit post"
+        )
 
 
-@app.delete("/delete/{id}")
+@app.delete("/{id}")
 def delete_post(req: Request, id: str = Path(...)):
     try:
         mongo = cast(MainMongo, req.app.state.mongo)
 
-        raw = mongo.conn["template"].find_one({"_id": id})
+        raw = mongo.db[__main_collection].find_one({"_id": id})
         if raw:
             payload = TypeAdapter(PostData).validate_python(raw)
 
-            mongo.conn["template"].update_one(
+            mongo.db[__main_collection].update_one(
                 {"_id": id}, {"$set": payload.delete_mongo}
             )
 
-            return payload.id
+            return JSONResponse(
+                status_code=status.HTTP_200_OK,
+                content={
+                    "status": status.HTTP_200_OK,
+                    "message": "Success delete post",
+                    "data": {"id": payload.id},
+                },
+            )
 
-        return JSONResponse(status_code=404, content="Data is not found")
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={
+                "status": status.HTTP_404_NOT_FOUND,
+                "message": "Post is not found",
+            },
+        )
     except Exception:
         logger.opt(exception=Exception).error("Failed delete post")
-        return HTTPException(status_code=500, detail="Failed delete post")
+        return HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Failed delete post",
+        )
